990b851898c45a4384bd2893f46fc1e0194dec76
unknown <daviddimas80@gmail.com>
2023-01-13 17:41:32 +0700

    set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'package:admin_pelayanan_katolik/DatabaseFolder/mongodb.dart';
+import 'package:admin_pelayanan_katolik/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp();
+  await MongoDatabase.connect();
+
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: Login(),
+  ));
 }
