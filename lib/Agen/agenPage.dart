6581d6d62751c7fc854d592d4a12bd0638a925ff
unknown <daviddimas80@gmail.com>
2023-01-15 13:51:52 +0700

    implement agent

---
+++ lib/Agen/agenPage.dart
@@ -0,0 +1,61 @@
+import 'package:admin_pelayanan_katolik/login.dart';
+import 'package:flutter/material.dart';
+
+import 'messages.dart';
+
+class AgenPage {
+  static var dataTampilan;
+  AgenPage() {
+    //measure
+    ReadyBehaviour();
+    //SendBehaviour();
+    ResponsBehaviour();
+  }
+  setDataTampilan(data) {
+    dataTampilan = data;
+  }
+
+  receiverTampilan() {
+    return dataTampilan;
+  }
+
+  ResponsBehaviour() async {
+    Messages msg = Messages();
+    var data = msg.receive();
+
+    action() async {
+      try {
+        if (data.runtimeType == List<Map<String, Object?>>) {
+          setDataTampilan(data);
+        }
+      } catch (error) {
+        return 0;
+      }
+    }
+
+    action();
+  }
+
+  ReadyBehaviour() {
+    Messages msg = Messages();
+    var data = msg.receive();
+    action() {
+      try {
+        if (data == "Done") {
+          return data;
+        }
+
+        if (data == "ready") {
+          runApp(MaterialApp(
+            title: 'Navigation Basics',
+            home: Login(),
+          ));
+        }
+      } catch (error) {
+        return 0;
+      }
+    }
+
+    action();
+  }
+}
---
+++ lib/Agen/agenPencarian.dart
@@ -0,0 +1,57 @@
+import 'dart:developer';
+
+import 'package:admin_pelayanan_katolik/DatabaseFolder/data.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+import '../DatabaseFolder/mongodb.dart';
+import 'messages.dart';
+
+class AgenPencarian {
+  AgenPencarian() {
+    ReadyBehaviour();
+    ResponsBehaviour();
+  }
+
+  ResponsBehaviour() {
+    Messages msg = Messages();
+
+    var data = msg.receive();
+    action() async {
+      try {
+        if (data.runtimeType == List<List<dynamic>>) {
+          if (data[0][0] == "cari Baptis") {
+            var userBaptisCollection =
+                MongoDatabase.db.collection(BAPTIS_COLLECTION);
+            await userBaptisCollection
+                .find({'idGereja': data[1][0], 'status': 0})
+                .toList()
+                .then((result) async {
+                  msg.addReceiver("agenPage");
+                  msg.setContent(result);
+                  await msg.send();
+                });
+          }
+        }
+      } catch (e) {
+        return 0;
+      }
+    }
+
+    action();
+  }
+
+  ReadyBehaviour() {
+    Messages msg = Messages();
+    var data = msg.receive();
+    action() {
+      try {
+        if (data == "ready") {
+          print("Agen Pencarian Ready");
+        }
+      } catch (e) {
+        return 0;
+      }
+    }
+
+    action();
+  }
+}
